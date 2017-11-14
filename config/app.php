898739a0d65a7de23582463b0b8e2254ae045ab8
artqz <djoctuk@yandex.ru>
2017-11-14 15:29:09 +0500

    Admin - create user

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
          */
 
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,5 +230,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 ];
